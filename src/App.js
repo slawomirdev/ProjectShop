76c3f93c30c6a634f979c4db7e7cc3600372fb64
thewilkupl <thewilku@o2.pl>
2021-07-13 11:22:05 +0200

    2.Better router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,46 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
-import { Home } from "./pages";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      <h4>comfy sloth starter</h4>;
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
