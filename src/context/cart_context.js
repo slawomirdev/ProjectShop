192cd16ef5827f8eb3b70f45eeb6fd42e20196c1
thewilkupl <thewilku@o2.pl>
2021-07-17 15:13:52 +0200

    remove item/clear from cart

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -34,12 +34,16 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
   //remove item from cart
-  const removeItem = (id) => {};
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
   //togle amount
   const toggleAmount = (id, value) => {};
   //clear cart
 
-  const clearCart = () => {};
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(state.cart));
