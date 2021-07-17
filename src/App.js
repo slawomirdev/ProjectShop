8b819eb3e6fc85ed5b657e01be918f6f1bab2af5
thewilkupl <thewilku@o2.pl>
2021-07-17 19:33:28 +0200

    fixing redirect checkout bug

--- src/App.js
+++ src/App.js
@@ -11,37 +11,40 @@ import {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper,
 } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
 
-        <Route exact path="/products">
-          <Products />
-        </Route>
-        <Route exact path="/products/:id" children={<SingleProduct />} />
-        <PrivateRoute exact path="/checkout">
-          <Checkout />
-        </PrivateRoute>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProduct />} />
+          <PrivateRoute exact path="/checkout">
+            <Checkout />
+          </PrivateRoute>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
