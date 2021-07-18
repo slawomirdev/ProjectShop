26827587b923d3037f0583d3666fd5fde316ce9a
thewilkupl <thewilku@o2.pl>
2021-07-18 23:20:52 +0200

    fix warnings

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -26,6 +26,7 @@ const SingleProductPage = () => {
 
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`);
+    // eslint-disable-next-line
   }, [id]);
 
   //Po 3 sekundach wracamy na główna strone
@@ -36,6 +37,7 @@ const SingleProductPage = () => {
         history.push("/");
       }, 3000);
     }
+    // eslint-disable-next-line
   }, [error]);
 
   if (loading) {
